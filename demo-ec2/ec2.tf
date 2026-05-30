447c75305eaa8e98c69b04fa03c11c4b19a8c6fd
Jyoti <ilu29jan@gmail.com>
2026-05-30 23:57:21 +0530

    file updated

--- demo-ec2/ec2.tf
+++ demo-ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-instance" {
     ami            = "ami-0fcc78c828f981df2"
     instance_type  = "t3.micro" 
 
-    tags {
+    tags = {
         Name = "terraform-instance"
     }
 }

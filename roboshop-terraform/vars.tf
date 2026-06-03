9fbab9b8533f4f217f2d612ea77b7313db1d3759
Jyoti <ilu29jan@gmail.com>
2026-06-03 15:24:24 +0530

    file updated

--- roboshop-terraform/vars.tf
+++ roboshop-terraform/vars.tf
@@ -1,19 +1,19 @@
 # Map Input : Map Of Maps
 variable "components" {
     default = {
-        catalogue = {
-            instance_type = "t3.micro"
-            ami_id = "ami-0fcc78c828f981df2"
-            user_data     = "components/frontend.sh"
-        }
+        # catalogue = {
+        #     instance_type = "t3.micro"
+        #     ami_id = "ami-0fcc78c828f981df2"
+        #     user_data     = "components/frontend.sh"
+        # }
         # shipping = {
         #     instance_type = "t3.micro"
         #     ami_id = "ami-0fcc78c828f981df2"
         # }
-        # frontend = {
-        #     instance_type = "t3.micro"
-        #     ami_id = "ami-0fcc78c828f981df2"
-        # }
+        frontend = {
+            instance_type = "t3.micro"
+            ami_id = "ami-0fcc78c828f981df2"
+        }
         # cart = {
         #     instance_type = "t3.micro"
         #     ami_id = "ami-0fcc78c828f981df2"

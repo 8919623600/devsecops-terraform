b26765d566a67ca7197c4d07a279c688afe0a3cf
Jyoti <ilu29jan@gmail.com>
2026-06-05 23:20:57 +0530

    file updated

---
+++ variables.tf/var.tf
@@ -0,0 +1,10 @@
+variable "cityname" {
+    type          = string
+    description   = "The name of the city"
+    default       = "bangalore"
+}
+
+output cityname {
+    value = var.cityname
+  
+}

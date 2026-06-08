cc38fe8c9c32590c92f05310209a0bd627d32774
Jyoti <ilu29jan@gmail.com>
2026-06-08 23:59:49 +0530

    file updated

--- modules/main.tf
+++ modules/main.tf
@@ -1,16 +1,3 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "roboshop-terraform"
-    key    = "statefile/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 data "aws_ami" "latest" {
     most_recent = true
     
